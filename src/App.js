ad447e996d9ca40081f42dd19edfd2a3ce33792d
Rohan-Swain <rohanswain2003@gmail.com>
2022-09-12 11:19:28 +0530

    Final commit: DONE

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Header from './components/Header';
+import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 function App() {
+  const initialState = JSON.parse(localStorage.getItem('tasks')) || [];
+  const [input, setInput] = useState('');
+  const [tasks, setTasks] = useState(initialState);
+  const [editTask, setEditTask] = useState(null);
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+
   return (
-    <div>
-      Crud App
+    <div className='container'>
+      <div className='app-wrapper'>
+        <div>
+          <Header />
+        </div>
+        <div>
+          <Form
+            input = {input}
+            setInput = {setInput}
+            tasks = {tasks}
+            setTasks = {setTasks} 
+            editTask = {editTask}
+            setEditTask = {setEditTask}
+          />
+        </div>
+        <div>
+          <TodoList tasks={tasks} setTasks={setTasks} setEditTask={setEditTask} />
+        </div>
+      </div>
     </div>
   );
 }
